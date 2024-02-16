4dac7bdfbef886713307ae28c4d56dac9bf56b3a
AnnaS <samohina-97@mail.ru>
2024-02-16 22:23:14 +0700

    final_homework_2_2

--- Final_Homework/2_2/Program.cs
+++ Final_Homework/2_2/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+//function
+void printNumber(int m, int n) { 
+    if(m>n)
+        return;
+    if (m % 2 == 0) {
+        Console.Write($"{m},");
+    }
+    printNumber(m+1,n);
+}
+
+
+Console.Write("Lesson 7.2.2\n");
+
+Console.Write("Enter natural number M:");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter natural number N:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if(m<=0 || n<=0)
+    Console.Write("Not a natural numbers:");
+
+printNumber(m,n);
